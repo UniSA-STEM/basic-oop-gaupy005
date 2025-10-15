182c52908e19471ef43cdf7ae7826af8cdb71618
gaupy005 <gaupy005@mymail.unisa.edu.au>
2025-10-15 19:21:26 +1030

    Updated Headers for each files

--- Hacker.py
+++ Hacker.py
@@ -1,8 +1,8 @@
 """
 File: Hacker.py
 Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Author: <Prince Gautam>
+ID: <110351192>
+Username: <GAUPY005>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
