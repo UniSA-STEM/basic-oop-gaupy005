f198de2336e039521c45faed634310a8b0e09231
gaupy005 <gaupy005@mymail.unisa.edu.au>
2025-10-15 22:17:59 +1030

    Finished creating an asset class using encrypt / decrypt and also did string output

--- Asset.py
+++ Asset.py
@@ -6,3 +6,38 @@ ID: <110351192>
 Username: <GAUPY005>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+
+class Asset:
+
+    # This makes an asset (token, drive, chip etc.)
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.encrypted = False
+
+    # This makes it encrypted
+    def encrypt(self):
+        self.encrypted = True
+
+    # This to turn of encrytpion
+    def decrypt(self):
+        self.encrypted = False
+
+    # This is to show the status if it's encrypted or not
+    def __str__(self):
+        if self.encrypted == True
+            return self.name + ":" + self.description + " encrypted"
+        else:
+            return self.name + ":" + self.description
+
+
+
+
+
+
+
+
+
+
+
