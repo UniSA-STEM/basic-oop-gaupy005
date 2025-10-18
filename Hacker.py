4794af1003cca8182c13824d8b92d46af6b18b3a
gaupy005 <gaupy005@mymail.unisa.edu.au>
2025-10-18 20:16:38 +1030

    Added Hacker class constructor with private attributes

--- Hacker.py
+++ Hacker.py
@@ -6,3 +6,12 @@ ID: <110351192>
 Username: <GAUPY005>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+class Hacker:
+    # this creates a hacker with a name, trace level and inventory
+    def __init__(self, name):
+        self.__name = name
+        self.__trace_level = 0
+        self.__inventory = []
+        self.__rig = None
+
